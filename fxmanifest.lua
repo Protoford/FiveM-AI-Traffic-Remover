337e73d71f8ee35cccc44fbc47eb7129cdce8387
Proto Ford <58895172+Protoford@users.noreply.github.com>
2022-01-30 08:54:33 -0800

    Update version number
    from 1.0.0 to 1.0.1 to 1.0.2

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ games { 'rdr3', 'gta5' }
 
 author 'Ziggy'
 description 'Traffic reducer (all values can be changed in cl.lua to your liking :)'
-version '1.0.0'
+version '1.0.2'
 
 -- What to run
 client_scripts {
