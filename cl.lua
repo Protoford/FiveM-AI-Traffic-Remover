98ef45f0fc1d68e338476d24005b39898453d687
Proto Ford <58895172+Protoford@users.noreply.github.com>
2022-01-30 08:45:35 -0800

    additional variables
    though more things to adjust takes away the simplicity of the original.

--- cl.lua
+++ cl.lua
@@ -1,20 +1,21 @@
-
--- change these to your liking :) (Ped frequency is the AI's walking Etc. and Traffic freq is the ammount of parked or driving vehicles)
+-- change these to your liking :) 
+-- (Ped frequency is the AI's walking Etc. and Traffic freq is the ammount of parked or driving vehicles)
 local config = {
+    pedDensity = 0.8,
     pedFrequency = 1.0,
-    trafficFrequency = 0.5
+    trafficFrequency = 0.5,
+    parkingFrequenxy = 0.3,
+    vehicleDensity = 0.4
 }
 
 
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
-        SetPedDensityMultiplierThisFrame(config.pedFrequency)
+        SetPedDensityMultiplierThisFrame(config.pedDensity)
         SetScenarioPedDensityMultiplierThisFrame(config.pedFrequency, config.pedFrequency)
-
-
         SetRandomVehicleDensityMultiplierThisFrame(config.trafficFrequency)
-        SetParkedVehicleDensityMultiplierThisFrame(config.trafficFrequency)
-        SetVehicleDensityMultiplierThisFrame(config.trafficFrequency)
+        SetParkedVehicleDensityMultiplierThisFrame(config.parkingFrequency)
+        SetVehicleDensityMultiplierThisFrame(config.vehicleDensity)
     end
 end)
